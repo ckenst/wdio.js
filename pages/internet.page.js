8e8312dee86fb1af7cb1b0eeb7bd7db2ab27675f
Chris Kenst <ckenst@gmail.com>
2020-03-17 14:55:33 -0700

    Adding more functionality

--- pages/internet.page.js
+++ pages/internet.page.js
@@ -17,30 +17,67 @@ class Internet {
   get childElements() {
     return this.parent.$$("li");
   }
-
-  get FirstLink() {
+  get firstLink() {
     return $("ul li:nth-child(1) a");
   }
-
+  link(index) {
+    return $(`ul li:nth-child(${index}) a`);
+  }
+  get username() {
+    return $("#username");
+  }
+  get password() {
+    return $("#password");
+  }
+  /**
+   * Enter the username into the field
+   * @param {String} text username to be entered
+   */
+  enterUsername(text) {
+    this.username.waitForDisplayed();
+    this.username.setValue(text);
+  }
+  /**
+   * Enter the password into the field
+   * @param {String} text password to be entered
+   */
+  enterPassword(text) {
+    this.password.waitForDisplayed();
+    this.password.setValue(text);
+  }
   specificChildElement(index) {
     return this.parent.$(`li:nth-child(${index})`);
   }
-
+  /**
+   * Clicks on the link based on the index provided
+   * @param {Number} index the index of the element
+   */
+  clickLink(index) {
+    this.link(index).waitForDisplayed();
+    this.link(index).click();
+  }
+  checkboxes(index) {
+    return $(`#checkboxes input:nth-child(${index})`);
+  }
+  clickCheckbox(index) {
+    this.checkboxes(index).waitForDisplayed();
+    this.checkboxes(index).click();
+  }
   getLiText() {
     this.childElements.filter(element => {
       console.log(element.getText());
     });
   }
-
   getSpecificElementText(index) {
+    this.specificChildElement(index).waitForDisplayed();
     return this.specificChildElement(index).getText();
   }
-
   clickOnLInk() {
-      if(this.FirstLink.isDisplayed() === true) {
-          this.FirstLink.click()
-      }
-      browser.pause(5000)
+    if (this.firstLink.isDisplayed() === true) {
+      this.firstLink.click();
+    }
+    this.h3Header.waitForDisplayed();
   }
 }
+
 module.exports = new Internet();
--- test/elementActions.test.js
+++ test/elementActions.test.js
@@ -5,9 +5,39 @@ describe('Test element actions', () => {
         browser.url('/')
         internetPage.clickOnLInk()
         expect(browser.getUrl()).equals('http://the-internet.herokuapp.com/abtest')
-    })
+    });
 
     it('Should get text', () => {
+        browser.url('/')
+        expect(internetPage.getSpecificElementText(1)).equals('A/B Testing')
+    });
 
-    })
+    it('should click checkbox', () => {
+        internetPage.clickLink(6)
+        internetPage.clickCheckbox(1)
+        expect(internetPage.checkboxes(1).isSelected()).equals(true)
+    });
+
+    it('should uncheck checkbox', () => {
+        internetPage.clickCheckbox(1)
+        expect(internetPage.checkboxes(1).isSelected()).equals(false)
+    });
+
+    it('Should enter username', () => {
+        browser.url(`/login`)
+        internetPage.enterUsername('chris');
+        assert.equal('chris', internetPage.username.getValue());
+    });
+
+    it('Should enter password', () => {
+        browser.url(`/login`)
+        internetPage.enterPassword('Password');
+        assert.equal('Password', internetPage.password.getValue());
+    });
+
+    it('Should clear value', () => {
+        internetPage.username.click();
+        internetPage.username.clearValue(); 
+        assert.equal('', internetPage.username.getValue());
+    });
 });
