75d486a23a8b6507fd55a954d6a5266f807c156d
Chris Kenst <ckenst@gmail.com>
2020-03-23 17:37:35 -0700

    New tests for ease

---
+++ test/kenst.test.js
@@ -0,0 +1,18 @@
+describe("kenst.com loads", function() {
+  it("with the right size", () => {
+    let size = browser.getWindowSize();
+    console.log("size is" + size); //this returns an object that I'm not sure how to read, lol
+  });
+
+  it("with the right title", () => {
+    browser.url("https://www.kenst.com/");
+    let text = browser.getTitle();
+    console.log("text is ", text);
+    assert.equal("Chris Kenst -", text);
+  });
+
+  it("has the right url", () => {
+    console.log("hello friend");
+    expect(browser.getUrl()).equals("https://www.kenst.com/");
+  });
+});
